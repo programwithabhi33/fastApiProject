988f6704fdbf2deec21dd52e8acdc3ca55e15c19
abhi <programwithabhi@gmail.com>
2023-06-05 05:59:40 +0530

    project completed with bootstrap alert

--- routes/note.py
+++ routes/note.py
@@ -12,12 +12,18 @@ templates = Jinja2Templates(directory="templates")
 @note.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     notes = client.fastApiNotes.notes.find()
-    return templates.TemplateResponse("index.html", {"request": request,"note":notes})
+    return templates.TemplateResponse("index.html", {"request": request,"notes":list(notes)})
 
 @note.get("/addNote", response_class=HTMLResponse)
 async def addNote(request: Request):
     return templates.TemplateResponse("add.html",{"request": request})
 
 @note.post("/")
-async def save_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def create_item(request: Request):
+    form = await request.form()
+    formDict = dict(form)
+    print(formDict)
+    formDict["important"] = True if "important" in formDict else False  # type: ignore
+    note = client.fastApiNotes.notes.insert_one(formDict)
+    notes = client.fastApiNotes.notes.find()
+    return templates.TemplateResponse("index.html", {"request": request,"notes":notes,"add_note":True})
